96cdc2727b9fe22d77320cf5a5a80ebcf9e11f2e
ewami <efili1989@gmail.com>
2023-11-06 22:31:45 +0100

    Minor changes

--- components/Navigation/index.tsx
+++ components/Navigation/index.tsx
@@ -8,14 +8,14 @@ const navigationItems = ["ABOUT", "BOOKS", "AUTHORS", "CONTACT"];
 const Navigation = () => {
     return (
         <header className="top-0 sticky z-10 bg-white">
-            <nav className="h-24 flex text-orange-600 text-md border-b-2 border-orange-500 items-center">
+            <nav className="h-32 flex text-orange-600 text-xl border-b-2 border-orange-500 items-center">
                 <a className="w-16 mx-4 max-lg:w-20" href="#">
                     <Image src={logo} width={100} height={100} alt="Logo" />
                 </a>
-                <div className="flex gap-11 px-6 max-sm:px-3 justify-center w-full">
+                <div className="flex px-6 max-sm:px-3 justify-center w-full h-full">
                     <MobileNavButton />
-                    <div className="hidden lg:block">
-                        <ul className="flex items-center gap-11">
+                    <div className="hidden lg:flex border-x-2 border-orange-500 px-6">
+                        <ul className="flex items-center gap-24">
                             {navigationItems.map((item: string) => (
                                 <NavigationItem
                                     key={item}
