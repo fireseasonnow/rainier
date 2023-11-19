73d8eda8d18831a83f78b152f798d854d97a5f48
ewami <efili1989@gmail.com>
2023-11-19 21:21:12 +0100

    Change nav colors and some other styling + minor changes

--- components/Navigation/NavigationItem.tsx
+++ components/Navigation/NavigationItem.tsx
@@ -15,8 +15,8 @@ const NavigationItem = (props: Props) => {
             <a
                 className={`block transition duration-150 ease-in-out ${
                     isActive
-                        ? "border-b-2 border-orange-500 text-orange-500"
-                        : "hover:border-b-2 hover:border-orange-500 focus:border-b-2 focus:border-orange-500 hover:scale-110 hover:text-orange-500"
+                        ? "border-b-2 md:border-orange-500 md:text-orange-500 border-white text-white"
+                        : "hover:border-b-2 md:hover:border-orange-500 focus:border-b-2 md:focus:border-orange-500 hover:scale-110 md:hover:text-orange-500 hover:text-white focus:text-white hover:border-white focus:border-white"
                 }`}
                 href="#!"
                 onClick={onClick}
--- components/Navigation/index.tsx
+++ components/Navigation/index.tsx
@@ -6,40 +6,6 @@ import { useState } from "react";
 
 const navigationItems = ["HOME", "ABOUT", "BOOKS", "AUTHORS", "CONTACT"];
 
-// const Navigation = () => {
-//     const [isActive, setIsActive] = useState("HOME");
-//     const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
-
-//     const handleMobileMenuToggle = () => {
-//         setMobileMenuOpen(!isMobileMenuOpen);
-//     };
-
-//     return (
-//         <header className="top-0 sticky z-10">
-//             <nav className="h-32 flex text-orange-600 text-xl border-b-2 border-orange-600 items-center">
-//                 <div className="flex px-6 max-sm:px-3 justify-center w-full h-full">
-//                     <MobileNavButton  isOpen={isMobileMenuOpen}
-//                         onClick={handleMobileMenuToggle}/>
-//                     <div className="hidden lg:flex border-x-2 border-orange-600 px-6">
-//                         <ul className="flex items-center gap-24">
-//                             {navigationItems.map((item: string) => (
-//                                 <NavigationItem
-//                                     key={item}
-//                                     name={item}
-//                                     isActive={isActive === item}
-//                                     onClick={()=>setIsActive(item)}
-//                                 />
-//                             ))}
-//                         </ul>
-//                     </div>
-//                 </div>           
-//             </nav>
-//         </header>
-//     );
-// };
-
-// export default Navigation;
-
 
 const Navigation = () => {
     const [isActive, setIsActive] = useState("HOME");
@@ -49,33 +15,32 @@ const Navigation = () => {
         setMobileMenuOpen(!isMobileMenuOpen);
     };
 
+    const handleNavItemClick = (item: string) => {
+        setIsActive(item);
+    };
+
+    const mappedNavItems = navigationItems.map((item: string) => (
+        <NavigationItem
+            key={item}
+            name={item}
+            isActive={isActive === item}
+            onClick={() => handleNavItemClick(item)} 
+        />
+    ));
+
     return (
         <header className="top-0 sticky z-10">
-            <nav className="h-32 flex text-orange-600 text-xl border-b-2 border-orange-600 items-center">
-                <div className="flex px-6 max-sm:px-3 justify-center w-full h-full">
+            <nav className="h-32 flex text-orange-600 text-xl border-b-2 border-orange-600">
+                <div className="flex px-6 max-sm:px-3 md:justify-center justify-end w-full h-full">
                     <MobileNavButton isOpen={isMobileMenuOpen} onClick={handleMobileMenuToggle} />
-                    <div className={`lg:hidden ${isMobileMenuOpen ? "block" : "hidden"}`}>
-                        <ul className="flex flex-col items-center gap-4 bg-orange-600 text-white w-full h-screen absolute top-0 left-0 pt-10 mt-32">
-                            {navigationItems.map((item: string) => (
-                                <NavigationItem
-                                    key={item}
-                                    name={item}
-                                    isActive={isActive === item}
-                                    onClick={() => setIsActive(item)}
-                                />
-                            ))}
+                    <div className={`md:hidden ${isMobileMenuOpen ? "block" : "hidden"}`}>
+                        <ul className="flex flex-col items-center gap-4 bg-orange-600 text-orange-100 w-full h-screen absolute top-0 left-0 pt-10 mt-32">
+                            {mappedNavItems}
                         </ul>
                     </div>
-                    <div className="hidden lg:flex border-x-2 border-orange-600 px-6">
+                    <div className="hidden md:flex border-x-2 border-orange-600 px-6">
                         <ul className="flex items-center gap-24">
-                            {navigationItems.map((item: string) => (
-                                <NavigationItem
-                                    key={item}
-                                    name={item}
-                                    isActive={isActive === item}
-                                    onClick={() => setIsActive(item)}
-                                />
-                            ))}
+                            {mappedNavItems}
                         </ul>
                     </div>
                 </div>
